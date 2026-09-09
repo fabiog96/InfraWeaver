0dbf5a548e2596ac4b7702c4d718e5cdb79aa695
Fabio Gioia <39191508+fabiog96@users.noreply.github.com>
2026-09-09 17:08:48 +0200

    chore: 🔥 remove unused MetadataEditor component (#22)

--- src/features/inspector/components/index.ts
+++ src/features/inspector/components/index.ts
@@ -1,6 +1,5 @@
 export { PropertyPanel } from './PropertyPanel';
 export { NodeForm } from './NodeForm';
-export { MetadataEditor } from './MetadataEditor';
 export { ColorPicker } from './ColorPicker';
 export { ModuleInputsForm } from './ModuleInputsForm';
 export { SecretsForm } from './SecretsForm';
